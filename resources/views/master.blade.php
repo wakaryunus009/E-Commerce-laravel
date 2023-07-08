8f78f275f8e80bd317008dbb76a4f52c66e7d1a0
wakaryunus009 <wakaryunus009@gamil.com>
2023-07-08 23:41:35 +0530

    first commit

---
+++ resources/views/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-Commerce-Shopping-Sites</title>
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+</head>
+<body>
+    {{View::make('header')}}
+    @yield('content')
+    {{View::make('footer')}}
+</body>
+
+
+<!-- <script>
+    $(document).ready(function (){
+        $("button").click(function() {
+          alert("ALL SET");  
+        })
+    })
+</script> -->
+
+
+<style>
+    .custom-login{
+        height:500px;
+        padding-top:100px;
+    }
+</style>
+</html>
